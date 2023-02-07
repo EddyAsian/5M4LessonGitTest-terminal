d9aa46ec0a6215cfdd129eafedb98d91957d2b2c
EddyAsian <lucky.wanderer.asian@gmail.com>
2023-02-08 04:34:29 +0600

    added names constant

--- 5M4LessonGitTest/ViewController.swift
+++ 5M4LessonGitTest/ViewController.swift
@@ -9,11 +9,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private let names = ["Ramazan", "Student"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print(names.count)
     }
-
-
 }
 
